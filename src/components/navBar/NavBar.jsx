56602d0cdf38247b9b2103bd827b94d39b27196f
Sara Baqla <missatrox44@gmail.com>
2023-04-01 09:57:35 -0500

    updated logo path

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar(){
     <nav class='bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-darkDesert'>
         <div class='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
             <a href='#' class='flex items-center'>
-                <img src='./public/dev-icon.svg' class='mr-3 h-6 sm:h-9' alt='Sara Dev Logo' />
+                <img src='./dev-icon.svg' class='mr-3 h-6 sm:h-9' alt='Sara Dev Logo' />
                 <span class='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>sarabaqla.dev</span>
             </a>
             <div class='flex items-center lg:order-2'>
