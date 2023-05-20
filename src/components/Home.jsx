0f94f1ba3bc3403d4cd5ee916904d0ad99e0cdc2
Sara Baqla <missatrox44@gmail.com>
2023-05-20 13:00:39 -0500

    updated home component image width

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 import landingImg from "/landing_img.svg";
-// import landingImg from '../../assets/seb-logo.svg';
 
 export default function Home() {
   return (
     <div id="home">
-      <img class="object-cover large-logo" src={landingImg} />
+      <img className="w-full" src={landingImg} alt="Landing" />
     </div>
   );
 }
