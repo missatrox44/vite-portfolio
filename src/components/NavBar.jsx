d217a11be695d1433553820287f810c8b84af1dd
Sara Baqla <missatrox44@gmail.com>
2023-05-20 13:19:58 -0500

    project section template added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -81,15 +81,6 @@ export default function NavBar() {
               </li>
               <li>
                 <HashLink
-                  to="#skills"
-                  onClick={() => setIsOpen(false)}
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Skills
-                </HashLink>
-              </li>
-              <li>
-                <HashLink
                   to="#projects"
                   onClick={() => setIsOpen(false)}
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
@@ -97,6 +88,16 @@ export default function NavBar() {
                   Projects
                 </HashLink>
               </li>
+              <li>
+                <HashLink
+                  to="#skills"
+                  onClick={() => setIsOpen(false)}
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Skills
+                </HashLink>
+              </li>
+              
               <li>
                 <HashLink
                   to="#experience"
