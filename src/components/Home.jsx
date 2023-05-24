16694f00687df0374c1dfba311e6de4fa43b0788
Sara Baqla <missatrox44@gmail.com>
2023-05-24 18:06:56 -0500

    updated logo spacing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
-// import landingImg from "/landing_img.svg";
 import landingImg from "/logo.svg";
-// import landingImg from "/transparent-logo.png";
-
 
 export default function Home() {
   return (
-    <div id="home">
+    <div id="home" className="bg-lightDesert py-12 px-8">
       <img className="w-full mt-12" src={landingImg} alt="Landing" />
     </div>
   );
