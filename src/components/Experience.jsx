ffc65471c21e696f10e06c1e73ef167063f349ec
Sara Baqla <missatrox44@gmail.com>
2023-06-06 20:14:31 -0500

    updated experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,6 +12,16 @@ const Experience = () => {
       img: "images/ibg.png",
     },
     {
+      title: "Software Developer",
+      company: "Chingu",
+      description: "group project and paired programming stuff",
+      timePeriod: "October 2022 - November 2022 & March 2023",
+      img: "images/chingu.png",
+    },
+  ];
+
+  const nonDevExperiences = [
+    {
       title: "Robotics Teacher",
       company: "ECISD",
       description:
@@ -60,15 +70,46 @@ const Experience = () => {
       img: "images/utep.png",
     },
   ];
-  
+
   return (
     <div
       id="experience"
       className="bg-lightDesert p-8 rounded-lg shadow-lg w-full mx-auto"
     >
-      <h2 className="text-4xl font-bold text-darkDesert mb-4 text-center">Experience</h2>
-      <div className="resume-section-content grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <h2 className="text-4xl font-bold text-darkDesert mb-4 text-center">
+        Experience
+      </h2>
+      <div className="resume-section-content grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
         {jobExperiences.map((experience, index) => (
+          <div
+            key={index}
+            className="bg-lightDesert p-6 border-2 border-darkDesert rounded-lg shadow-md hover:border-4 hover:border-darkDesert transition-all duration-500 ease-in-out"
+          >
+            <div className="flex items-center mb-2">
+              <h3 className="text-2xl font-bold text-darkDesert mr-4">
+                {experience.title}
+              </h3>
+              <img
+                src={experience.img}
+                alt={experience.title}
+                className="h-12 w-auto"
+              />
+            </div>
+            <p className="text-xl text-darkDesert italic mb-4">
+              {experience.company}
+            </p>
+            <p className="text-darkDesert hidden">{experience.description}</p>
+            <p className="text-darkDesert mt-4 font-bold">
+              {experience.timePeriod}
+            </p>
+          </div>
+        ))}
+      </div>
+      <h2 className="text-4xl font-bold text-darkDesert mb-4 text-center">
+        Non Dev Experience
+      </h2>
+      <div className="resume-section-content grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {nonDevExperiences.map((experience, index) => (
           <div
             key={index}
             className="bg-lightDesert p-6 border-2 border-darkDesert rounded-lg shadow-md hover:border-4 hover:border-darkDesert transition-all duration-500 ease-in-out"
@@ -95,7 +136,6 @@ const Experience = () => {
       </div>
     </div>
   );
-  
 };
 
 export default Experience;
