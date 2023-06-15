89f0f5ae3cadd123f5bcf024405172de7a9dd536
Sara Baqla <missatrox44@gmail.com>
2023-06-15 07:55:14 -0500

    installed react-router-dom, implemented routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import {
   NavBar,
   Home,
@@ -15,17 +17,21 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-lightDesert">
-      <NavBar />
-      <Home />
-      <About />
-      <Projects />
-      <Skills />
-      <Experience />
-      <Education />
-      <ContactMe />
-      <Footer />
-    </div>
+    <Router>
+      <div className="bg-lightDesert">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/contact" element={<ContactMe />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
