ab93a852333ce2da15029cf5a5628c1e0fb603a0
Sara Baqla <missatrox44@gmail.com>
2023-06-29 21:46:46 -0500

    added skills icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,26 +1,41 @@
-import React from "react";
 import {
   FaHtml5,
   FaCss3Alt,
-  FaJsSquare,
   FaReact,
   FaNodeJs,
   FaNpm,
+  FaBootstrap,
 } from "react-icons/fa";
-import { SiVisualstudiocode, SiMongodb, SiTailwindcss } from "react-icons/si";
+import {
+  SiVisualstudiocode,
+  SiMongodb,
+  SiTailwindcss,
+  SiMui,
+  SiTypescript,
+  SiJavascript,
+  SiYarn,
+  SiVite,
+  SiMysql,
+} from "react-icons/si";
 import { DiGithubBadge } from "react-icons/di";
 
 const skills = [
   { id: "html", Component: FaHtml5 },
   { id: "css", Component: FaCss3Alt },
-  { id: "javascript", Component: FaJsSquare },
+  { id: "javascript", Component: SiJavascript },
+  { id: "bootstrap", Component: FaBootstrap },
+  { id: "typescript", Component: SiTypescript },
   { id: "react", Component: FaReact },
-  { id: "node", Component: FaNodeJs },
-  { id: "npm", Component: FaNpm },
-  { id: "vscode", Component: SiVisualstudiocode },
-  { id: "mongodb", Component: SiMongodb },
-  { id: "github", Component: DiGithubBadge },
+  { id: "mui", Component: SiMui },
   { id: "tailwind", Component: SiTailwindcss },
+  { id: "node", Component: FaNodeJs },
+  { id: "sql", Component: SiMysql },
+  { id: "npm", Component: FaNpm },
+  { id: "yarn", Component: SiYarn },
+  { id: "mongodb", Component: SiMongodb },
+  { id: "vite", Component: SiVite },
+  { id: "vscode", Component: SiVisualstudiocode },
+  { id: "github", Component: DiGithubBadge },
 ];
 
 export default function Skills() {
@@ -30,7 +45,7 @@ export default function Skills() {
       className="bg-lightDesert rounded-lg shadow-md flex items-center justify-center min-h-screen"
     >
       <div className="text-center max-w-2xl w-full mx-auto">
-        <h2 className="text-4xl font-bold text-darkDesert mb-4">Expertise</h2>
+        <h2 className="text-4xl font-bold text-darkDesert mb-6">Expertise</h2>
         <div className="grid grid-cols-4 gap-4 my-4 justify-items-center">
           {skills.map(({ id, Component }) => (
             <div
