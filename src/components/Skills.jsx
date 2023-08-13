9393ef516622225e4eb36d59e0ed401fb0966f48
Sara Baqla <missatrox44@gmail.com>
2023-08-12 22:35:03 -0500

    added animation to skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,4 @@
+import { useTrail, animated } from "react-spring";
 import {
   FaHtml5,
   FaCss3Alt,
@@ -39,22 +40,31 @@ const skills = [
 ];
 
 export default function Skills() {
+  const trail = useTrail(skills.length, {
+    from: { transform: "translate3d(0,-40px,0)", opacity: 0 },
+    to: { transform: "translate3d(0,0px,0)", opacity: 1 },
+  });
+
   return (
     <div
       id="skills"
       className="bg-lightDesert rounded-lg shadow-md flex items-center justify-center min-h-screen"
     >
       <div className="text-center max-w-2xl w-full mx-auto">
-        <h2 className="text-4xl font-bold text-darkDesert mb-6">Expertise</h2>
+        <h2 className="text-4xl font-bold text-darkDesert mb-6">Skills</h2>
         <div className="grid grid-cols-4 gap-4 my-4 justify-items-center">
-          {skills.map(({ id, Component }) => (
-            <div
-              className="text-5xl text-goldDesert transition-all duration-300 transform hover:scale-110 hover:text-darkDesert"
-              key={id}
-            >
-              <Component />
-            </div>
-          ))}
+          {trail.map((props, index) => {
+            const { Component } = skills[index];
+            return (
+              <animated.div
+                style={props}
+                className="text-5xl text-goldDesert transition-all duration-300 transform hover:scale-110 hover:text-darkDesert"
+                key={skills[index].id}
+              >
+                <Component />
+              </animated.div>
+            );
+          })}
         </div>
       </div>
     </div>
