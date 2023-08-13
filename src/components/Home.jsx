42c461ff6571e637137b33abbf29eab78446c500
Sara Baqla <missatrox44@gmail.com>
2023-08-12 22:13:31 -0500

    added spring animation to landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 import {
   FaGithub,
   FaLinkedin,
@@ -16,64 +17,79 @@ import {
 } from "../constants";
 
 export default function Home() {
+  const logoSpring = useSpring({
+    from: { opacity: 0, transform: "scale(0.5)" },
+    to: { opacity: 1, transform: "scale(1)" },
+  });
+
+  const iconSpring = useSpring({
+    from: { opacity: 0, transform: "translateY(20px)" },
+    to: { opacity: 1, transform: "translateY(0)" },
+    config: { tension: 300, friction: 20 },
+    delay: 300,
+  });
+
   return (
     <div
       id="home"
       className="bg-lightDesert flex flex-col items-center justify-center min-h-screen"
     >
-      <img
+      <animated.img
+        style={logoSpring}
         className="w-fit mb-4"
         src={landingImg}
         alt="Trumpet with tentacle logo"
       />
 
-      <div className="flex mt-2 space-x-6">
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={GITHUB_URL}
-          className="text-darkDesert hover:text-goldDesert transition-colors duration-300"
-        >
-          <FaGithub className="w-10 h-10" />
-          <span className="sr-only">GitHub account</span>
-        </a>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={LINKEDIN_URL}
-          className="text-darkDesert hover:text-goldDesert transition-colors duration-300"
-        >
-          <FaLinkedin className="w-10 h-10" />
-          <span className="sr-only">LinkedIn account</span>
-        </a>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={YOUTUBE_URL}
-          className="text-darkDesert hover:text-goldDesert transition-colors duration-300"
-        >
-          <FaYoutube className="w-10 h-10" />
-          <span className="sr-only">YouTube Channel</span>
-        </a>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={RESUME_URL}
-          className="text-darkDesert hover:text-goldDesert transition-colors duration-300"
-        >
-          <FaFileAlt className="w-10 h-10" />
-          <span className="sr-only">Resume</span>
-        </a>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={BLOG_URL}
-          className="text-darkDesert hover:text-goldDesert transition-colors duration-300"
-        >
-          <FaBlog className="w-10 h-10" />
-          <span className="sr-only">Blog</span>
-        </a>
-      </div>
+      <animated.div style={iconSpring} className="flex mt-2 space-x-6">
+        <div className="flex mt-2 space-x-6">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={GITHUB_URL}
+            className="text-darkDesert hover:text-goldDesert transition-colors duration-300"
+          >
+            <FaGithub className="w-10 h-10" />
+            <span className="sr-only">GitHub account</span>
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={LINKEDIN_URL}
+            className="text-darkDesert hover:text-goldDesert transition-colors duration-300"
+          >
+            <FaLinkedin className="w-10 h-10" />
+            <span className="sr-only">LinkedIn account</span>
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={YOUTUBE_URL}
+            className="text-darkDesert hover:text-goldDesert transition-colors duration-300"
+          >
+            <FaYoutube className="w-10 h-10" />
+            <span className="sr-only">YouTube Channel</span>
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={RESUME_URL}
+            className="text-darkDesert hover:text-goldDesert transition-colors duration-300"
+          >
+            <FaFileAlt className="w-10 h-10" />
+            <span className="sr-only">Resume</span>
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={BLOG_URL}
+            className="text-darkDesert hover:text-goldDesert transition-colors duration-300"
+          >
+            <FaBlog className="w-10 h-10" />
+            <span className="sr-only">Blog</span>
+          </a>
+        </div>
+      </animated.div>
     </div>
   );
 }
