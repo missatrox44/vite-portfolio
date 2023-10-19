4ed24f88d98c19f85636dd1e2db66be17d063ade
Sara Baqla <missatrox44@gmail.com>
2023-10-19 07:52:37 -0500

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import {
   NavBar,
   Home,
@@ -11,29 +11,32 @@ import {
   ContactMe,
   Footer,
   NotFound,
-} from "../src/components/index";
+} from '../src/components/index';
 
-import "./index.css";
-import "./App.css";
+import './index.css';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="bg-lightDesert">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/contact" element={<ContactMe />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      <Analytics />
+      <Router>
+        <div className="bg-lightDesert">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/contact" element={<ContactMe />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </>
   );
 }
 
