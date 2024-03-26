9a6e20bd4cb0d6c2457d4ff2aa1617974f22ccea
Sara Baqla <missatrox44@gmail.com>
2024-03-26 09:17:01 -0500

    added container to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -109,7 +109,7 @@ export default function Projects() {
   return (
     <div
       id="projects"
-      className="flex flex-col items-center p-8 bg-lightDesert mt-12 min-h-screen"
+      className="container mx-auto flex flex-col items-center p-8 bg-lightDesert mt-12 min-h-screen"
     >
       <h2 className="text-4xl font-bold text-darkDesert mb-4">Projects</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
