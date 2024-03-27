842af21997b8a8d423870fa6fd64038d440fa767
Sara Baqla <missatrox44@gmail.com>
2024-03-27 08:57:11 -0500

    added container to home

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
     <animated.div
       style={fadeIn}
       id="about"
-      className="flex flex-col items-center bg-lightDesert p-8 md:mx-6 mt-12"
+      className="container mx-auto flex flex-col items-center bg-lightDesert p-8 mt-12"
     >
       <animated.img
         style={{ ...springProps }}
