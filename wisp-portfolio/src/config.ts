5bbefabe008a6938d10cfc31d726dac7085a6376
Sara Baqla <missatrox44@gmail.com>
2025-01-30 06:12:30 -0600

    installed wisp next app base template

---
+++ wisp-portfolio/src/config.ts
@@ -0,0 +1,33 @@
+const buildConfig = () => {
+  const blogId = process.env.NEXT_PUBLIC_BLOG_ID;
+  if (!blogId) throw new Error("NEXT_PUBLIC_BLOG_ID is missing");
+  const name = process.env.NEXT_PUBLIC_BLOG_DISPLAY_NAME || "Development.";
+  const copyright = process.env.NEXT_PUBLIC_BLOG_COPYRIGHT || "Artemis Bytes";
+  const defaultTitle =
+    process.env.NEXT_DEFAULT_METADATA_DEFAULT_TITLE || "Web Solutions by Artemis Bytes";
+  const defaultDescription = process.env.NEXT_PUBLIC_BLOG_DESCRIPTION || "Blog about business solutions in the digital world.";
+
+  return {
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
+    blog: {
+      name,
+      copyright,
+      metadata: {
+        title: {
+          absolute: defaultTitle,
+          default: defaultTitle,
+          template: `%s - ${defaultTitle}`,
+        },
+        description: defaultDescription,
+      },
+    },
+    ogImageSecret:
+      process.env.OG_IMAGE_SECRET ||
+      "secret_used_for_signing_and_verifying_the_og_image_url",
+    wisp: {
+      blogId,
+    },
+  };
+};
+
+export const config = buildConfig();
